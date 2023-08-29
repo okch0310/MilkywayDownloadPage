eedb51ab1d73097b77a123c37ca8dc81833aef23
okch0310 <okch0310@naver.com>
2023-08-29 11:01:35 +0900

    db connect, data render with table

--- back/index.js
+++ back/index.js
@@ -44,7 +44,7 @@ const client = new Client({
 client.connect();
 
 client
-  .query('SELECT * FROM dashff.user_log LIMIT 2')
+  .query('SELECT * FROM dashff.user_log LIMIT 5')
   .then((res) => {
     data=res.rows;
     console.log("excute query:",data)
