a4508d38ced06ea2737a86312470743ad4466436
okch0310 <okch0310@naver.com>
2023-08-29 20:51:15 +0900

    각 버튼 클릭시 해당하는 os 다운 받아지도록

--- back/index.js
+++ back/index.js
@@ -29,7 +29,7 @@
 
 import pkg from 'pg';
 const { Client } = pkg;
-var data=""
+const data=[];
 
 function runIndex(){
 
@@ -43,12 +43,49 @@ const client = new Client({
 
 client.connect();
 
+// 첫번째 쿼리 실행 (Windows)
 client
-  .query('SELECT * FROM dashff.mw_version')
+  .query(`select *
+  from dashff.mw_version
+  where 1=1
+    and corp_cd ='KO'
+    and os='Windows'
+    and latest_yn =true
+  order by create_dt desc
+  limit 1`)
   .then((res) => {
-    data=res.rows;
-    console.log("excute query:",data)
+    data.push(res.rows);
+    console.log("Windows Query result:",res.rows);
+
+    // 두번째 쿼리 실행 (M1, M2)
+    return client.query(`select *
+    from dashff.mw_version
+    where 1=1
+      and corp_cd ='KO'
+      and os='M1, M2 Mac'
+      and latest_yn =true
+    order by create_dt desc
+    limit 1`);
+  })
+  .then((res)=> {
+    data.push(res.rows);
+    console.log("M1, M2 Query result:", res.rows);
+
+    // 세번째 쿼리 실행 (Intel)
+    return client.query(`select *
+    from dashff.mw_version
+    where 1=1
+      and corp_cd ='KO'
+      and os='Intel Mac'
+      and latest_yn =true
+    order by create_dt desc
+    limit 1`);
+  })
+  .then((res)=> {
+    data.push(res.rows);
+    console.log("Intel Query result:", res.rows);
     client.end();
+    console.log("All queries executed:", data);
   })
   .catch((e) => console.error(e.stack));
 }
