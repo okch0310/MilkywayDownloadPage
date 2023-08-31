4f456e63e35ed8f90558b75db3a774bb9e01535c
okch0310 <okch0310@naver.com>
2023-08-31 10:46:50 +0900

    hover text colort 등 css

--- back/index.js
+++ back/index.js
@@ -1,32 +1,3 @@
-// import pkg from 'pg';
-// const { Client } = pkg;
-// var data=""
-
-// function runIndex(){
-
-// const client = new Client({
-//   user: "data_user",
-//   host: "prd-dt-redshift.conhugwtudej.ap-northeast-2.redshift.amazonaws.com",
-//   database: "fnf",
-//   password: "Duser2022!#",
-//   port: 5439,
-// });
-
-// client.connect();
-
-// client
-//   .query('SELECT * FROM dashff.user_log LIMIT 3')
-//   .then((res) => {
-//     data=res.rows;
-//     console.log("console:",data)
-//     client.end();
-//   })
-//   .catch((e) => console.error(e.stack));
-
-// }
-
-// export {runIndex, data}
-
 import pkg from 'pg';
 const { Client } = pkg;
 const data=[];
@@ -83,10 +54,21 @@ client
   })
   .then((res)=> {
     data.push(res.rows);
+    console.log("Version History:", res.rows);
+
+    // Update History data  
+    return client.query(`select *
+    from dashff.mw_version
+    order by version desc`);
+  })
+  
+  .then((res)=> {
+    data.push(res.rows);
     console.log("Intel Query result:", res.rows);
     client.end();
     console.log("All queries executed:", data);
   })
+  
   .catch((e) => console.error(e.stack));
 }
 
--- back/server.js
+++ back/server.js
@@ -1,27 +1,3 @@
-// import express from 'express';
-// import path from 'path';
-// import { runIndex } from "./index.js";
-
-
-// const app = express()
-// const port = 3000
-// const __dirname = path.resolve();
-// const publicDir = path.join(__dirname, '..', 'front');
-
-// app.use(express.static(publicDir)); 
-
-// app.get('/', (req, res) => { 
-//   runIndex()
-//   res.sendFile(path.join(__dirname, '..', 'front', 'index.html'));
-//   //console.log("dd",data)
-// })
-
-
-
-// app.listen(port, () => {
-//   console.log(`server on >>>>>> http://localhost:${port}`)
-// })
-
 import express from 'express';
 import path from 'path';
 import { runIndex,data } from "./index.js";
