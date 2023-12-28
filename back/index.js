52b40070ca95a3aaf1477cfc9343465c7e0c6aaf
okch0310 <okch0310@naver.com>
2023-12-28 16:48:46 +0900

    기억 안나는 여러 수정사항들..

--- back/index.js
+++ back/index.js
@@ -1,35 +1,36 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Client } = pkg;
-const data=[];
+const data = [];
 
-function runIndex(){
+function runIndex() {
+  const client = new Client({
+    user: "dashff_user",
+    host: "prd-dt-redshift.conhugwtudej.ap-northeast-2.redshift.amazonaws.com",
+    database: "fnf",
+    password: "Dashff2022!#",
+    port: 5439,
+  });
 
-const client = new Client({
-  user: "dashff_user",
-  host: "prd-dt-redshift.conhugwtudej.ap-northeast-2.redshift.amazonaws.com",
-  database: "fnf",
-  password: "Dashff2022!#",
-  port: 5439,
-});
+  client.connect();
 
-client.connect();
-
-// 첫번째 쿼리 실행 (Windows)
-client
-  .query(`select *
+  // 첫번째 쿼리 실행 (Windows)
+  client
+    .query(
+      `select *
   from dashff.mw_version
   where 1=1
     and corp_cd ='KO'
     and os='Windows'
     and latest_yn =true
   order by create_dt desc
-  limit 1`)
-  .then((res) => {
-    data.push(res.rows);
-    console.log("Windows Query result:",res.rows);
+  limit 1`
+    )
+    .then((res) => {
+      data.push(res.rows);
+      console.log("Windows Query result:", res.rows);
 
-    // 두번째 쿼리 실행 (M1, M2)
-    return client.query(`select *
+      // 두번째 쿼리 실행 (M1, M2)
+      return client.query(`select *
     from dashff.mw_version
     where 1=1
       and corp_cd ='KO'
@@ -37,13 +38,13 @@ client
       and latest_yn =true
     order by create_dt desc
     limit 1`);
-  })
-  .then((res)=> {
-    data.push(res.rows);
-    console.log("M1, M2 Query result:", res.rows);
+    })
+    .then((res) => {
+      data.push(res.rows);
+      console.log("M1, M2 Query result:", res.rows);
 
-    // 세번째 쿼리 실행 (Intel)
-    return client.query(`select *
+      // 세번째 쿼리 실행 (Intel)
+      return client.query(`select *
     from dashff.mw_version
     where 1=1
       and corp_cd ='KO'
@@ -51,25 +52,25 @@ client
       and latest_yn =true
     order by create_dt desc
     limit 1`);
-  })
-  .then((res)=> {
-    data.push(res.rows);
-    console.log("Version History:", res.rows);
+    })
+    .then((res) => {
+      data.push(res.rows);
+      console.log("Version History:", res.rows);
 
-    // Update History data  
-    return client.query(`select *
+      // Update History data
+      return client.query(`select *
     from dashff.mw_version
     order by version desc`);
-  })
-  
-  .then((res)=> {
-    data.push(res.rows);
-    console.log("Intel Query result:", res.rows);
-    client.end();
-    console.log("All queries executed:", data);
-  })
-  
-  .catch((e) => console.error(e.stack));
+    })
+
+    .then((res) => {
+      data.push(res.rows);
+      console.log("Intel Query result:", res.rows);
+      client.end();
+      console.log("All queries executed:", data);
+    })
+
+    .catch((e) => console.error(e.stack));
 }
 
-export {runIndex, data}
+export { runIndex, data };
